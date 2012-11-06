9c7be3d51aa2dc0dea13c37f5f3c92f85ac30cf9
Shawn Seals <shawn.seals915@yahoo.com>
2012-11-06 11:30:26 -0600

    Initial Sector Control v1.0 Upload

---
+++ workspace/SectorControl_v1.0/src/com/sectorcontrol/GameDriver.java
@@ -0,0 +1,14 @@
+package com.sectorcontrol;
+
+public class GameDriver {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Gameboard board1 = new Gameboard(21, 15);
+		board1.printGameBoard();
+	}
+
+}
